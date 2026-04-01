7f7d1ba22f2173b207e77df3599e677aca80125c
wmacfarl <wmacfarl@gmail.com>
2026-04-01 14:12:51 -0400

    fix index.js and remove unused store.js

--- index.js
+++ index.js
@@ -1,29 +1,14 @@
-import mainView from "./views/main-view.js";
 import applicationStore from "./stores/application-store.js";
 /* index.js */
 import MainView from "./views/main-view.js";
-import store from "./store/store.js";
 
 const application = Choo();
-function getBasePath() {
-  return window.location.hostname === "wmacfarl.github.io"
-    ? "/stop-motion-station"
-    : "";
-}
-const path = getBasePath();
 application.use(applicationStore);
-application.route("*", mainView);
+application.route("*", MainView);
 
-export default application;
+window.requestAnimationFrame(() => {
+  application.emitter.emit("application:startup");
+});
 
-if (typeof window !== "undefined") {
-  application.mount("#app");
-
-  window.requestAnimationFrame(() => {
-    application.emitter.emit("application:startup");
-  });
-}
-application.route(`/${path}`, MainView);
-console.log(`🛣️  Mounted main view at /${path}/`);
 application.mount("#app");
 window.application = application;
