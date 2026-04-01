c83b5316c0e05db27fef2b146e2a53a71ad9f8fb
wmacfarl <wmacfarl@gmail.com>
2026-04-01 13:41:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 /* index.js */
-import APP_CONFIG from "./data/app-config.js";
 import MainView from "./views/main-view.js";
 import store from "./store/store.js";
 
@@ -13,9 +12,7 @@ function getBasePath() {
 const path = getBasePath();
 choo.use(store);
 
-// browser
 choo.route(`/${path}`, MainView);
 console.log(`🛣️  Mounted main view at /${path}/`);
-// individual games
 choo.mount("#app");
 window.choo = choo;
