994bf57f808e209f121168c98dc64231530ef2de
wmacfarl <wmacfarl@gmail.com>
2026-04-01 14:38:57 -0400

    Reorganize bootstrap and centralize store wiring

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-import store from "./stores/store.js";
-/* index.js */
-import MainView from "./views/main-view.js";
+import applicationStore from "./app.js";
+import mainView from "./views/main-view.js";
 
 const application = Choo();
-application.use(store);
-application.route("*", MainView);
+application.use(applicationStore);
+application.route("*", mainView);
+application.mount("#app");
 
 window.requestAnimationFrame(() => {
   application.emitter.emit("application:startup");
 });
 
-application.mount("#app");
 window.application = application;
