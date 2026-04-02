764d10120fededa11b4b04e42377b192efcb4075
wmacfarl <wmacfarl@gmail.com>
2026-04-02 18:21:23 -0400

    Map ArrowUp keyboard shortcut to playback start

--- app.js
+++ app.js
@@ -165,6 +165,12 @@ export default function applicationStore(state, emitter) {
       if (keyPressed === "ArrowRight") {
         keyboardEvent.preventDefault();
         emitter.emit("timeline:move-selection-right");
+        return;
+      }
+
+      if (keyPressed === "ArrowUp") {
+        keyboardEvent.preventDefault();
+        emitter.emit("playback:start");
       }
     };
 
